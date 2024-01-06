e38aa34d6c0d45927611b1f79b38f445431b861a
rxn7 <rotthin_dev@protonmail.com>
2024-01-06 06:31:00 +0100

    Added Label class for readability

---
+++ label.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "U8g2lib.h"
+
+class Label {
+public:
+    Label(const uint8_t y, U8G2 &u8g) : m_U8g(u8g), m_PosY(y) {
+    }
+
+    void set_text(const char *text) {
+        m_Text = text;
+        m_PosX = 64 - m_U8g.getStrWidth(text) / 2;
+    }
+
+    inline void render() {
+        m_U8g.drawStr(m_PosX, m_PosY, m_Text);
+    }
+
+private:
+    const char *m_Text;
+    uint8_t m_PosX;
+    uint8_t m_PosY;
+    U8G2 &m_U8g;
+};
--- nano_bird.ino
+++ nano_bird.ino
@@ -1,63 +1,58 @@
-#include "U8g2lib.h"
+#include <U8g2lib.h>
+
 #include "player.h"
 #include "pipe.h"
+#include "label.h"
 
 constexpr uint8_t BUTTON_PIN = 2;
-const char *SCORE_TEXT_PREFIX = "Score: ";
-const char *WAITING_FOR_INPUT_TEXT = "Tap to play";
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g(U8G2_R0);
 
-uint8_t waitingForInputTextPosX = 0;
-uint8_t scoreStartScreenTextPosX = 0;
-uint8_t scoreTextPosX = 0;
+Label scoreStartScreenLabel(32, u8g);
+Label waitingForInputLabel(48, u8g);
+Label scoreLabel(64, u8g);
 
-float scrollSpeed = PIPE_SCROLL_SPEED;
 uint16_t score = 0;
+float scrollSpeed = PIPE_SCROLL_SPEED;
 bool startScreenRendered = false;
 bool waitingForInput = true;
-
-char scoreStartScreenTextBuffer[14] = "\0";
-char scoreTextBuffer[6] = "\0";
 
 Player player;
 Pipe pipe;
 
 void setup() {
-    strcpy(scoreStartScreenTextBuffer, SCORE_TEXT_PREFIX);
+	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	pinMode(LED_BUILTIN, OUTPUT);
 
     u8g.setPowerSave(true);
     u8g.begin();
     u8g.setFont(u8g2_font_profont12_tr);
 
-    waitingForInputTextPosX = 64 - u8g.getStrWidth(WAITING_FOR_INPUT_TEXT) / 2;
-
-	pinMode(BUTTON_PIN, INPUT_PULLUP);
-	pinMode(LED_BUILTIN, OUTPUT);
+    waitingForInputLabel.set_text("Tap to play");
 
     reset();
 }
 
 void reset() {
-    static size_t prefixSize = strlen(SCORE_TEXT_PREFIX);
-    snprintf(scoreStartScreenTextBuffer + prefixSize, sizeof(scoreStartScreenTextBuffer) - prefixSize, "%u", score);
+    static char scoreStartScreenTextBuffer[14] = "\0";
+    snprintf(scoreStartScreenTextBuffer, sizeof(scoreStartScreenTextBuffer), "Score: %u", score);
+    scoreStartScreenLabel.set_text(scoreStartScreenTextBuffer);
 
     scrollSpeed = PIPE_SCROLL_SPEED;
-    scoreStartScreenTextPosX = 64 - u8g.getStrWidth(scoreStartScreenTextBuffer) / 2;
 
     waitingForInput = true;
     startScreenRendered = false;
-	score = 0;
+    set_score(0);
 
 	player = {};
     pipe.reset();
-
-    update_score_text();
 }
 
-void update_score_text() {
-    scoreTextPosX = 64 - u8g.getStrWidth(scoreTextBuffer);
+void set_score(const uint8_t newScore) {
+    score = newScore;
+    static char scoreTextBuffer[6] = "\0";
     snprintf(scoreTextBuffer, sizeof(scoreTextBuffer), "%u", score);
+    scoreLabel.set_text(scoreTextBuffer);
 }
 
 void loop() {
@@ -76,8 +71,8 @@ void loop() {
         if(!startScreenRendered) {
             startScreenRendered = true;
 
-            u8g.drawStr(waitingForInputTextPosX, 48, WAITING_FOR_INPUT_TEXT);
-            u8g.drawStr(scoreStartScreenTextPosX, 32, scoreStartScreenTextBuffer);
+            waitingForInputLabel.render();
+            scoreStartScreenLabel.render();
 
             goto display_and_ret;
         }
@@ -107,13 +102,12 @@ void loop() {
 
 
     if(!pipe.m_Scored && pipe.m_PosX + PIPE_WIDTH / 2 <= PLAYER_POS_X) {
-        ++score;
-        update_score_text();
+        set_score(score + 1);
         pipe.m_Scored = true;
         scrollSpeed += PIPE_SCROLL_SPEED_INCREMENT;
     }
 
-    u8g.drawStr(scoreTextPosX, 64, scoreTextBuffer);
+    scoreLabel.render();
 
     pipe.draw(u8g);
     player.draw(u8g);
