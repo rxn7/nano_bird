8836e60a087d024e57f867fc9640ea96897f3478
rxn7 <rotthin_dev@protonmail.com>
2024-01-06 02:50:21 +0100

    Fixed formatting

--- player.h
+++ player.h
@@ -11,33 +11,33 @@ constexpr float JUMP_FORCE = -GRAVITY * 200.0f;
 
 class Player {
 public:
-  void update(const uint16_t deltaTime) {
+	void update(const uint16_t deltaTime) {
 	m_Velocity += deltaTime * GRAVITY;
 	float positionChange = m_Velocity * deltaTime;
 
 	if (positionChange > UINT8_MAX - m_PosY || m_PosY + positionChange >= FLOOR_POS_Y) {
-	  m_Dead = true;
-	  return;
+		m_Dead = true;
+		return;
 	}
 
 	m_PosY += positionChange;
-  }
+	}
 
-  void draw(U8GLIB &u8g) {
-	u8g.drawBox(m_PosX, m_PosY, PLAYER_SIZE_X, PLAYER_SIZE_Y);
-  }
+	void draw(U8GLIB &u8g) {
+		u8g.drawBox(m_PosX, m_PosY, PLAYER_SIZE_X, PLAYER_SIZE_Y);
+	}
 
-  void jump() {
-	m_Velocity = JUMP_FORCE;
-  }
+	void jump() {
+		m_Velocity = JUMP_FORCE;
+	}
 
-  inline bool is_dead() const {
-	return m_Dead;
-  }
+	inline bool is_dead() const {
+		return m_Dead;
+	}
 
 private:
-  bool m_Dead = false;
-  float m_Velocity = 0;
-  uint8_t m_PosY = 10;
-  uint8_t m_PosX = 20;
+	bool m_Dead = false;
+	float m_Velocity = 0;
+	uint8_t m_PosY = 10;
+	uint8_t m_PosX = 20;
 };
